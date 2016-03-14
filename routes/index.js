a12f1c6beeb51448cd85a9a97641f7eed33292f2
Cameron Bryers <cambo_the_rambo@hotmail.com>
2016-03-14 21:59:42 +1300

    Second Page Added
    - Added a second page, which is to be used for searching the database. Need to return search results now.

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,14 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 
 client.execute("OPEN Colenso");
 // client.execute("xquery //movie[position() 
-router.get("/",function(req, res, next){
+
+router.get("/",function(req, res, next) {
 client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
 " (//name[@type='place'])[1] ",
 function (error, result) {
   if(error){ console.error(error);}
   else {
-    res.render('index', { title: 'ECS Video Rental', place: result.result });
+    res.render('index', { title: 'The Colenso Project', place: result.result });
   }
     }
     );
