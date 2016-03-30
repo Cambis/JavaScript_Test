2a856a29e3464b6e88c01e45c94cdf051dc3c2a8
Cameron Bryers <cambo_the_rambo@hotmail.com>
2016-03-30 22:07:58 +1300

    Massive Overhaul
    - Re-arranged the code in search
    - Plain search files can now be accessed
    - Need to fix this for specific xquery searches

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ var http = require('http');
 
 var isXQuery = false;
 
+
+var DEFAULT_NAMESPACE = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';";
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -18,8 +20,8 @@ client.execute("OPEN Colenso");
 // client.execute("xquery //movie[position() 
 
 router.get("/", function(req, res, next) {
-client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-" (//name[@type='place'])[1] ",
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+"/TEI[. contains text 'natives' ftand 'Elizabeth']",
   function (error, result) {
     console.log('Searched');
     console.log(result.result);
@@ -37,79 +39,69 @@ router.get('/results', function(req, res, next) {
   // XQUERY doc('Colenso_TEIs/" + path + "')";
   
   var input = decodeInput(req.query.srch + "?" + (isXQuery ? "xquery" : "plain"));
-  var path = req.query.path;
   
   // client.execute("XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
   // "(collection('Colenso_TEIs/Colenso/private_letters')//tei:p[position() = 1])",
   
   console.log("INPUT: " + input + " URL: " + fullUrl(req));
-  console.log("PATH: " + path);
   
 //   // If there is a search
   if (input != null && input.length > 0) {
     
     console.log("SEARCHING GHEE..." + input);
-    
-    client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-     input,
-    
-    function(error, result) {
-      
-      console.log("HEY XQUERY IS " + isXQuery + " eh");
+  
+    // Check if it is a xml document 
+    client.execute("XQUERY db:is-xml('Colenso', '" + input + "')", 
+    function(error, xmlResult) {
       
       if (error)
         console.log(error);
-      else
-        console.log("SEARCH RESULT: " + result.result);
       
-      // Get the filepath and filename of the document
-      var filepath = result.result;
-      var filename = filepath.split("/")[(filepath.split("/").length - 1)];
-      
-      // Check if it is a xml document 
-      client.execute("XQUERY db:is-xml('Colenso', '" + result.result + "')", 
-      function(error, xmlResult) {
-        if (error)
-          console.log(error);
-        else
-          console.log("IS XML? " + result.result);
+      if (xmlResult.result == 'true') {
         
-        if (xmlResult.result == 'true') {
-          isXML = true;
-          console.log("OMG, XML!!!");
-          client.execute("XQUERY doc('Colenso_TEIs/" + result.result + "')",
-          function(error, result) {
-            if (error)
-              console.log(error);
-            else 
-              console.log(result);
-            
-	    console.log("FILEPATH: " + filepath);
-	    console.log("FILENAME: " + filename);
-	    
-            res.render('file', {title: 'Search Archives', content: result.result, path: filepath, name: filename });
-            return;
-          });
-        } else {
-                
+        isXML = true;
+        
+        console.log("OMG, XML!!!");
+        
+        // Get the filepath and filename of the document
+        var filepath = input;
+        var filename = filepath.split("/")[(filepath.split("/").length - 1)];
+        
+        client.execute("XQUERY doc('Colenso_TEIs/" + input + "')",
+        function(error, result) {
+        
+          if (error)
+            console.log(error);
+          else 
+            console.log(result);
+ 
+          console.log("OKAY OKAY O FUCKING KAY");
+          res.render('file', {title: 'Search Archives', content: result.result, path: filepath, name: filename });
+          return;
+        });
+      } else {
+        
+        console.log("                      NOT AN XML");
+        // Otherwise continue searching
+        client.execute(input, function(error, result) {
+        
+          if (error)
+            console.log("ERROR:" + error);
+          else
+            console.log("SEARCH RESULT: " + result.result);
+          
           var resultsArray = result.result.split("\n");
-          console.log("ENTRIES...");
-          
-          resultsArray.forEach(function(entry) {
-            console.log(entry);
-          });
-          
-          res.render('results', { title: 'Search Archives', res: resultsArray, srch: req.query.srch, path: path});
-        }
-      });
-    }); 
+        
+          res.render('results', { title: 'Search Archives', res: resultsArray, srch: req.query.srch});        
+        });
+      }
+   });
   }
   // No search
   else {
-    res.render('results', { title: 'Search Archives', res: '', srch: '', path: '' });
+    res.render('results', { title: 'Search Archives', res: '', srch: ''});
     console.log("NO SEARCH");
   }
-  
 });
 
 /* BROWSE */
@@ -160,36 +152,6 @@ router.get("/browse", function(req, res, next) {
     res.render('browse', { title: 'Browse', authors: authors});
   });
 });
-
-// XXX: do not use me
-// "for $n in (//title = title//) return db:path($n)"
-function findPathOfAuthor(author) {
-  
-  console.log("AUTHOR: " + author);
-  
-  path = "for $n in (//title[../author/name[@type='person' and .='" + author + "']]/text())\n" +
-"return db:path($n)"
-
-  console.log("PATH: " + path);
-  
-  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-  path, 
-  function(error, result) {
-    console.log("SEARCHINGS... ");
-    if (!error) {
-      console.log("IN HERE");
-      var beh = result.result.split("\n")
-      var eh = beh[0].split("/");
-      console.log("EH: " + eh[0]);
-      return eh[0];
-    }
-    if (error) 
-      console.log("HOLY SHIT, HOLY SHIT " + error);
-  });
-  
-  console.log("Got here");
-  return path;
-}
 
 /* DOWNLOAD */
 router.get('/download/:name', function(req, res) {
@@ -266,23 +228,66 @@ function decodeInput(search) {
   else if (contains(search, "?title")) {
     
     console.log("SEARCH IS ?TITLE");
+    // Remove tags
+    search = removeTags(search);
+    // first check if it is an xml
     
-    var titleArray = search.split("?");
-    var title = titleArray[0];
+//     console.log("SEARCH IS THIS: " + search);
+//     
+//     // Shit we need to make it a path
+//     if (!contains(search, "/")) {
+//       
+//       console.log("EHHHHHHH");
+//       
+//       client.execute(DEFAULT_NAMESPACE + "for $file in collection('Colenso')" +
+//            "where $file //title[.= '" + title + "']" +
+//            "return db:path($file)",
+//       function(err, res) {
+//         if (!err)
+//           search = res.result;
+//         console.log("SEARCH IS: " + search + " HERE");
+//         
+//         return false;
+//       });
+//     }
+//     
+//     console.log("BUT: " + search + " HERE");
+//     
+//     client.execute("XQUERY db:is-xml('Colenso', '" + search + "')",
+//     function(error, result) {
+//       if (!error) {
+//         console.log("This is it boy!!!!");
+//         return "XQUERY doc('Colenso_TEIs/" + search + "')";
+//       }
+//     });
+//     
+//     console.log("SEARCH IS ?TITLE");
     
-    console.log("TITLE: " + title);
-    // return "for $n in (//title[.= " + title + "]/text()) return db:path($n)"
-  
-   return "for $file in collection('Colenso')" +
-          "where $file //title[.= '" + title + "']" +
-          "return db:path($file)";
-    
+//     if (!contains(search, "/"))
+//       return "for $file in collection('Colenso')" +     
+//              "where $file //title[.= '" + search + "']" +
+//              "return db:path($file)";
+      
+      return search;
+//     var title = search;
+//     console.log("TITLE: " + title);
+//     // return "for $n in (//title[.= " + title + "]/text()) return db:path($n)"
+//   
+//     return "for $file in collection('Colenso')" +
+//            "where $file //title[.= '" + title + "']" +
+//            "return db:path($file)";
+//     
   }
   
   // XQuery (?xquery)
   else if (contains(search, "?xquery")) {
     console.log("SEARCH IS ?XQUERY");
-    return search.replace("?xquery", "");
+    
+    // Adds namespace for noobs
+    var beh = (!contains(search, DEFAULT_NAMESPACE)) ? DEFAULT_NAMESPACE + search.replace("?xquery", "") 
+    : search.replace("?xquery", "");
+    
+    return beh;
   }
   
   // Plain Text (?plain)
@@ -290,19 +295,45 @@ function decodeInput(search) {
      console.log("SEARCH IS ?PLAIN");
      
      var query = search.replace("?plain", "");
+     console.log("SEARCHING FOR: " + query);
+     //  "$file //author/name[.= '" + query + "'])" +
      
-     return "for $file in collection('Colenso')" +
-            "where $file //title[.= '" + query + "']" || 
-            "$file //author/name[.= '" + query + "']" ||
-            "return db:path($file)";
+     var beh = "for $file in (/TEI[. contains text '" + query + "' using wildcards])" + 
+               "return db:path($file)";
+     
+     // beh = "/TEI[. contains text '" + query + "']";
+     
+//      client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+//      query, function(err, res) {
+//        if (!err)
+//          console.log(res);
+//      });
+           
+     return DEFAULT_NAMESPACE + beh;
+     
+     // "/TEI[. contains text 'natives' ftand 'Elizabeth']"
+     // (//title[../author/name[@type='person' and .='Joseph Dalton Hooker']]/text())
   }
   
   return search;
 }
 
+function removeTags(string) {
+  if (contains(string, "?xquery"))
+    string = string.replace("?xquery", "");
+  
+  if (contains(string, "?plain"))
+    string = string.replace("?plain", "");
+  
+  if (contains(string, "?title"))
+    string = string.replace("?title", "");
+  
+  return string;
+}
+
 /* Decode the search to see what key it contains */
 function contains(string, substring) {
-  console.log("SEARCH: " + string);
+  // console.log("SEARCH: " + string);
   // var urlArray = search.split("?");
   // return urlArray.length > 2 && (urlArray.indexOf(item) > -1);
   return string.indexOf(substring) > -1;
